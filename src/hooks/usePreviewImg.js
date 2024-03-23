5332c9ba84590c66a280123265766e4411e3a5df
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-03-23 08:58:24 +0530

    image resizer added

--- src/hooks/usePreviewImg.js
+++ src/hooks/usePreviewImg.js
@@ -1,25 +1,40 @@
 import { useState } from "react";
 import useShowToast from "./useShowToast";
+import Resizer from "react-image-file-resizer"
 
 const usePreviewImg = () => {
-	const [imgUrl, setImgUrl] = useState(null);
-	const showToast = useShowToast();
-	const handleImageChange = (e) => {
-		const file = e.target.files[0];
-		if (file && file.type.startsWith("image/")) {
-			const reader = new FileReader();
+  const [imgUrl, setImgUrl] = useState(null);
+  const showToast = useShowToast();
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file && file.type.startsWith("image/")) {
+      const reader = new FileReader();
 
-			reader.onloadend = () => {
-				setImgUrl(reader.result);
-			};
+      reader.onloadend = () => {
+        Resizer.imageFileResizer(
+          file,
+          150,
+          150,
+          file.type === "image/jpeg" ? "JPEG" : "PNG",
+          100,
+          0,
+          (resizedImg) => {
+            setImgUrl(resizedImg);
+            //   console.log(resizedImg);
+          },
+          "base64"
+        );
+		//   setImgUrl(reader.result);
+		  console.log(reader)
+      };
 
-			reader.readAsDataURL(file);
-		} else {
-			showToast("Invalid file type", " Please select an image file", "error");
-			setImgUrl(null);
-		}
-	};
-	return { handleImageChange, imgUrl, setImgUrl };
+      reader.readAsDataURL(file);
+    } else {
+      showToast("Invalid file type", " Please select an image file", "error");
+      setImgUrl(null);
+    }
+  };
+  return { handleImageChange, imgUrl, setImgUrl };
 };
 
 export default usePreviewImg;
